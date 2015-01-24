95803008bae3d7cc2d40715eba1f13503202c4fa
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-24 03:07:11 -0200

    made refactory on moving the player

--- app/script/main.js
+++ app/script/main.js
@@ -1,58 +1,18 @@
-// var Player = require('./player.js');
-var Phaser = window.Phaser;
+var Player = require('./player.js');
 
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-var sprite;
-var cursors;
-
 var game = new window.Phaser.Game(width, height, window.Phaser.AUTO, '', {
   preload: function() {
     game.load.image('max', 'image/max.png');
-    // game.load.spritesheet('max', 'image/max.png', 32, 48);
   },
 
   create: function() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    cursors = game.input.keyboard.createCursorKeys();
-
-    sprite = game.add.sprite(200, 200, 'max');
-
-    game.physics.enable(sprite, Phaser.Physics.ARCADE);
-    
-    //  This gets it moving
-    sprite.body.velocity.setTo(0, 0);
-    
-    //  This makes the game world bounce-able
-    sprite.body.collideWorldBounds = true;
-    
-    //  This sets the image bounce energy for the horizontal 
-    //  and vertical vectors (as an x,y point). "1" is 100% energy return
-    sprite.body.bounce.setTo(0, 0);
+    Player.initialize();
   },
 
   update: function() {
-    if (cursors.up.isDown)
-    {
-        sprite.body.acceleration.y = -600;
-    }
-    else if (cursors.down.isDown)
-    {
-        sprite.body.acceleration.y = 600;
-    }
-    // else if (cursors.left.isDown)
-    // {
-    //     sprite.body.acceleration.x = -500;
-    // }
-    // else if (cursors.right.isDown)
-    // {
-    //     sprite.body.acceleration.x = 500;
-    // }
-    else
-    {
-        sprite.frame = 4;
-        sprite.body.acceleration.setTo(0,0);
-    }
+    Player.initialize();
   }
 });
--- app/script/player.js
+++ app/script/player.js
@@ -1,40 +1,30 @@
 var Player = {
   initialize: function(game) {
-    this.player = game.add.sprite(200, 200, 'max');
-    game.physics.arcade.enable(this.player);
-
-    // this.player.body.bounce.y = 0.2;
-    // this.player.body.gravity.y = 100;
-    this.player.body.collideWorldBounds = true;
-
-    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+    game.physics.startSystem(window.Phaser.Physics.ARCADE);
 
     this.cursors = game.input.keyboard.createCursorKeys();
+    this.player = game.add.sprite(200, 200, 'max');
+
+    game.physics.enable(this.player, window.Phaser.Physics.ARCADE);
+
+    this.player.body.velocity.setTo(0, 0);
+    this.player.body.collideWorldBounds = true;
+    this.player.body.bounce.setTo(0, 0);
   },
 
   walk: function() {
-    this.player.body.velocity.x = 100;
-
     if (this.cursors.up.isDown) {
-      this.player.body.velocity.y = 150;
+      this.player.body.acceleration.y = -600;
+      return;
+    } 
+
+    if (this.cursors.down.isDown) {
+      this.player.body.acceleration.y = 600;
+      return;
     }
-    // if (this.cursors.left.isDown) {
-    //   this.player.body.velocity.x = -150;
-    //   this.player.animations.play('left');
 
-    //   return;
-    // }
-
-    // if (this.cursors.right.isDown) {
-    //   this.player.body.velocity.x = 150;
-    //   this.player.animations.play('right');
-
-    //   return;
-    // }
-
-    // this.player.animations.stop();
-    // this.player.frame = 4;
+    this.player.frame = 4;
+    this.player.body.acceleration.setTo(0,0);
   }
 };
 
