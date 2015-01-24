1b0d10414da4d95bd77a13596311b5c8c66e759f
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-24 02:42:03 -0200

    improved player physic

--- app/script/main.js
+++ app/script/main.js
@@ -1,21 +1,49 @@
-var Player = require('./player.js');
+// var Player = require('./player.js');
 var Phaser = window.Phaser;
 
 var width = window.innerWidth;
 var height = window.innerHeight;
 
+var sprite;
+var cursors;
+
 var game = new window.Phaser.Game(width, height, window.Phaser.AUTO, '', {
   preload: function() {
     game.load.image('max', 'image/max.png');
-    // game.load.spritesheet('max', 'image/max.png', 48, 48);
+    // game.load.spritesheet('max', 'image/max.png', 32, 48);
   },
 
   create: function() {
-    game.physics.startSystem(Phaser.Physics.ARCATE);
-    Player.initialize(game);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    //  Make things a bit more bouncey
+    game.physics.p2.defaultRestitution = 0.8;
+
+    //  Add a sprite
+    sprite = game.add.sprite(200, 200, 'max');
+
+    //  Enable if for physics. This creates a default rectangular body.
+    game.physics.p2.enable(sprite);
+
+    //  Modify a few body properties
+    sprite.body.setZeroDamping();
+    sprite.body.fixedRotation = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    // game.physics.startSystem(Phaser.Physics.ARCATE);
+    // Player.initialize(game);
   },
 
   update: function() {
-    Player.walk();
+    sprite.body.setZeroVelocity();
+
+    if (cursors.up.isDown) {
+      sprite.body.moveUp(400);
+    } else if (cursors.down.isDown) {
+      sprite.body.moveDown(400);
+    }
+
+    // Player.walk();
   }
 });
--- app/script/player.js
+++ app/script/player.js
@@ -1,10 +1,10 @@
 var Player = {
   initialize: function(game) {
-    this.player = game.add.sprite(32, game.world.height - 150, 'max');
+    this.player = game.add.sprite(200, 200, 'max');
     game.physics.arcade.enable(this.player);
 
-    this.player.body.bounce.y = 0.2;
-    this.player.body.gravity.y = 300;
+    // this.player.body.bounce.y = 0.2;
+    // this.player.body.gravity.y = 100;
     this.player.body.collideWorldBounds = true;
 
     this.player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -14,24 +14,27 @@ var Player = {
   },
 
   walk: function() {
-    this.player.body.velocity.x = 0;
+    this.player.body.velocity.x = 100;
 
-    if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -150;
-      this.player.animations.play('left');
-
-      return;
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y = 150;
     }
+    // if (this.cursors.left.isDown) {
+    //   this.player.body.velocity.x = -150;
+    //   this.player.animations.play('left');
 
-    if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = 150;
-      this.player.animations.play('right');
+    //   return;
+    // }
 
-      return;
-    }
+    // if (this.cursors.right.isDown) {
+    //   this.player.body.velocity.x = 150;
+    //   this.player.animations.play('right');
 
-    this.player.animations.stop();
-    this.player.frame = 4;
+    //   return;
+    // }
+
+    // this.player.animations.stop();
+    // this.player.frame = 4;
   }
 };
 
