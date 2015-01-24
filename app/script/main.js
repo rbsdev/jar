ca770304c93bab86af64cb3ddd73de1479e5e546
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-24 01:48:01 -0200

    created player class

--- app/script/main.js
+++ app/script/main.js
@@ -1,8 +1,8 @@
-//var preload = require('./preload.js');
-//var create = require('./create.js');
-
+var Player = require('./player.js');
 var Phaser = window.Phaser;
-var cursors;
+// var cursors;
+// var player;
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   preload: function() {
     game.load.spritesheet('max', 'image/dude.png', 32, 48);
@@ -10,22 +10,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 
   create: function() {
     game.physics.startSystem(Phaser.Physics.ARCATE);
-
-    var player = game.add.sprite(32, game.world.height - 150, 'max');
-    game.physics.arcade.enable(player);
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-   cursors = game.input.keyboard.createCursorKeys();
+    Player.initialize(game);
   },
 
   update: function() {
-    //if (cursors.left.isDown) {
-      //player.body.velocity.x
-    //}
+    Player.walk();
   }
 });
--- app/script/player.js
+++ app/script/player.js
@@ -1,3 +1,38 @@
-var Player = {};
+var Player = {
+  initialize: function(game) {
+    this.player = game.add.sprite(32, game.world.height - 150, 'max');
+    game.physics.arcade.enable(this.player);
+
+    this.player.body.bounce.y = 0.2;
+    this.player.body.gravity.y = 300;
+    this.player.body.collideWorldBounds = true;
+
+    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+  },
+
+  walk: function() {
+    this.player.body.velocity.x = 0;
+    // console.log(this.cursor);
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -150;
+      this.player.animations.play('left');
+
+      return;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = 150;
+      this.player.animations.play('right');
+
+      return;
+    }
+
+    this.player.animations.stop();
+    this.player.frame = 4;
+  }
+};
 
 module.exports = Player;
