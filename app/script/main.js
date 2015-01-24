2aa0a58a49c991c3b71f6b63099fe2114d624dc6
Gibran Malheiros <gibatronic@gmail.com>
2015-01-24 07:19:59 -0200

    wrapped initialization code to control it

--- app/script/main.js
+++ app/script/main.js
@@ -6,36 +6,44 @@ var Interface = require('./interface.js');
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-var game = new window.Phaser.Game(width, height, window.Phaser.AUTO, '', {
-  preload: function() {
-    game.load.image('sky', 'image/sky.png');
-    game.load.image('max', 'image/max.png');
-  },
+window.main = function(user) {
+  var game = new window.Phaser.Game(width, height, window.Phaser.AUTO, '', {
+    preload: function() {
+      var userAvatarUrl = user && user.avatar_url ? user.avatar_url : 'image/max.png';
 
-  create: function() {
-    Scenario.initialize(game, width, height);
-    Movement.initialize(game);
-    Player.initialize(100,100,'Evandro');
-    Interface.initialize(game);
+      game.load.crossOrigin = 'anonymous';
+      game.load.image('sky', 'image/sky.png');
+      game.load.image('max', userAvatarUrl);
+    },
 
-    Interface.import({
-      name: 'timer',
-      module: require('./timer.js')
-    }).render('timer', '00:00:00');
+    create: function() {
+      var userName = user && user.name ? user.name : 'Max';
 
-    Interface.import({
-      name: 'life',
-      module: require('./life.js')
-    }).render('life', '100');
+      Scenario.initialize(game, width, height);
+      Movement.initialize(game);
+      Player.initialize(100, 100, userName);
 
-    Interface.import({
-      name: 'power',
-      module: require('./power.js')
-    }).render('power', '100');
-  },
+      Interface.initialize(game);
 
-  update: function() {
-    Scenario.walk();
-    Movement.render();
-  }
-});
+      Interface.import({
+        name: 'timer',
+        module: require('./timer.js')
+      }).render('timer', '00:00:00');
+
+      Interface.import({
+        name: 'life',
+        module: require('./life.js')
+      }).render('life', '100');
+
+      Interface.import({
+        name: 'power',
+        module: require('./power.js')
+      }).render('power', '100');
+    },
+
+    update: function() {
+      Scenario.walk();
+      Movement.render();
+    }
+  });
+};
