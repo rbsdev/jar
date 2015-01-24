7bdfaf637d26eebe9ff5c7cc3e4ec56325a25c8d
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-24 06:39:48 -0200

    improve movement method

--- app/script/movement.js
+++ app/script/movement.js
@@ -2,6 +2,7 @@ var Movement = {
   initialize: function(game, element) {
     this.element = element;
     this.game = game;
+    this.x = 10;
 
     this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
     this.game.physics.enable(this.element, window.Phaser.Physics.ARCADE);
@@ -9,8 +10,19 @@ var Movement = {
   },
 
   render: function() {
-    this.game.input.activePointer.x = false;
-    this.game.physics.arcade.moveToPointer(this.element, 50, this.game.input.activePointer, 400);
+    
+    if (this.game.input.keyboard.isDown(window.Phaser.Keyboard.SPACEBAR)) {
+      this.x += 2;
+    } else {
+      if (this.x <= 10) {
+        this.x = 10;
+      } else {
+        this.x -= 2;
+      }
+    }
+    
+    this.game.input.activePointer.x = this.x;
+    this.game.physics.arcade.moveToPointer(this.element, 1, this.game.input.activePointer, 400);
     return this;
   }
 };
--- app/script/spaceship.js
+++ app/script/spaceship.js
@@ -1,6 +1,7 @@
 var Spaceship = function(game) {
   this.game = game;
   this.element = this.game.add.sprite(200, 200, 'max');
+  this.element.anchor.setTo(0, 0.5);
   return this.element;
 };
 
