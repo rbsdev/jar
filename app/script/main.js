02f9ae3a2e9d3ab59d536ab90e1980548e706311
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-24 05:24:06 -0200

    created node server to solved problem with fucking cache

--- app/script/main.js
+++ app/script/main.js
@@ -1,15 +1,19 @@
 var Controller = require('./controller.js');
+// var Scenario = require('./scenario.js');
 
 var width = window.innerWidth;
 var height = window.innerHeight;
 
+// console.log('evandro');
 var game = new window.Phaser.Game(width, height, window.Phaser.AUTO, '', {
   preload: function() {
+    game.load.image('sky', 'image/sky.png');
     game.load.image('max', 'image/max.png');
   },
 
   create: function() {
     Controller.initialize(game);
+    // Scenario.initialize(game);
   },
 
   update: function() {
