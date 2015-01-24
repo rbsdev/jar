b5e673fc3ef676663beee83619e1e005436f4189
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-24 16:09:41 -0200

    created meteor group class

---
+++ app/script/meteor-group.js
@@ -0,0 +1,30 @@
+var MeteorGroup = function(amount, game) {
+  this.amount = amount;
+  this.game = game;
+  this.meteor = null;
+};
+
+MeteorGroup.prototype = {
+  descend: function() {
+    this.meteor.y += 10;
+  },
+
+  create: function() {
+    this.meteors = this.game.add.group();
+    this.meteors.enableBody = true;
+    this.meteors.physicsBodyType = window.Phaser.Physics.ARCADE;
+
+    var meteor1 = this.meteors.create(3*48, 3*50, 'meteor');
+    meteor1.body.moves = true;
+
+    var meteor2 = this.meteors.create(2*48, 2*50, 'meteor');
+    meteor2.body.moves = true;
+
+    var tween = this.game.add.tween(this.meteors).to( { x: 200 }, 2000, 
+                                window.Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+    tween.onLoop.add(this.descend, this);
+  }
+};
+
+module.exports = MeteorGroup;
