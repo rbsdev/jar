2b250a67dcc176472d00a3bb56e069f9ce9fca91
Evandro Eisinger <evandroeisinger@gmail.com>
2015-01-24 23:50:04 -0200

    refactoring

---
+++ app/script/elements.js
@@ -0,0 +1,29 @@
+var Elements = {
+  initialize: function(game) {
+    this.game     = game;
+    this.elements = {};
+    return this;
+  },
+
+  import: function(element) {
+    this.elements[element.name] = element.module.initialize(this.game);
+    return this;
+  },
+
+  render: function(element, value) {
+    this.elements[element].render(value);
+    return this;
+  },
+
+  update: function(element, value) {
+    this.elements[element].update(value);
+    return this;
+  },
+
+  delete: function(element, value) {
+    this.elements[element].delete(value);
+    return this;
+  }
+};
+
+module.exports = Elements;
