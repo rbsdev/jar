a719305ec2d451d2436905d8f96d7bedb98d1a14
Gibran Malheiros <gibatronic@gmail.com>
2015-01-25 03:43:17 -0200

    changed the temporary landing page for the official one

--- app/script/kickoff.js
+++ app/script/kickoff.js
@@ -1,46 +1,41 @@
-var assets,
-    ask,
-    askGithub,
+var ask,
+    assets,
     data = [ ],
+    dom,
     done,
-    error,
-    grab,
     index = 0,
     kickoff,
-    landing,
     load,
-    meter,
-    parse,
     progress,
     start,
-    total,
-    withGithub,
-    withoutGithub;
+    total;
 
 assets = [
   'vendor/phaser.min.js',
   'script/main.js',
-  'image/spaceship.png',
   'image/layer01.png',
   'image/layer02.png',
   'image/layer03.png',
-  'image/nave.png'
+  'image/nave.png',
+  'image/spaceship.png'
 ];
+
+dom = {
+  landing: '.landing',
+  skip: '.landing-skip',
+  use: '.landing-use'
+};
 
 total = assets.length;
 
 ask = function() {
-  withGithub.classList.add('hidden');
-  withoutGithub.classList.add('hidden');
 
-  askGithub.classList.remove('hidden');
-  askGithub.focus();
 };
 
 done = function() {
   var body = document.body;
 
-  landing.classList.add('landing-ready');
+  dom.landing.classList.add('landing-open');
 
   assets.forEach(function(asset, index) {
     var script;
@@ -56,38 +51,13 @@ done = function() {
   });
 };
 
-error = function() {
-  askGithub.disabled = false;
-};
-
-grab = function(event) {
-  var xhr;
-
-  if (event.which !== 13) {
-    return;
-  }
-
-  askGithub.disabled = true;
-
-  xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('error', error);
-  xhr.addEventListener('load', parse.bind(this, xhr));
-
-  xhr.open('GET', 'https://api.github.com/users/' + askGithub.value.trim());
-  xhr.send();
-};
-
 kickoff = function() {
-  askGithub = document.querySelector('.landing-ask-github');
-  landing = document.querySelector('.landing');
-  meter = document.querySelector('.landing-progress-meter');
-  withGithub = document.querySelector('.landing-with-github');
-  withoutGithub = document.querySelector('.landing-without-github');
+  dom.landing = document.querySelector(dom.landing);
+  dom.skip = document.querySelector(dom.skip);
+  dom.use = document.querySelector(dom.use);
 
-  askGithub.addEventListener('keyup', grab);
-  withGithub.addEventListener('click', ask);
-  withoutGithub.addEventListener('click', start);
+  dom.skip.addEventListener('click', start);
+  dom.use.addEventListener('click', ask);
 
   load();
 };
@@ -113,18 +83,6 @@ load = function(pxhr) {
   xhr.send();
 };
 
-parse = function(xhr) {
-  var data;
-
-  try {
-    data = JSON.parse(xhr.response);
-  } catch (error) {
-    error(error);
-  }
-
-  start(data);
-};
-
 progress = function(event) {
   var ratio;
 
@@ -132,16 +90,12 @@ progress = function(event) {
     return;
   }
 
-  ratio = ((event.loaded / event.total) * index) / total;
-  meter.style.right = ((1 - ratio) * 100).toFixed(5) + '%';
+  ratio = ((index - 1) + (event.loaded / event.total)) / total;
+  dom.landing.style.right = ((1 - ratio) * 100).toFixed(5) + '%';
 };
 
 start = function(data) {
-  withGithub.disabled = true;
-  withoutGithub.disabled = true;
-
-  landing.classList.add('hidden');
-
+  dom.landing.style.display = 'none';
   window.main(data);
 };
 
