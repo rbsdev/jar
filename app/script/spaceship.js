332269d9c6a67b3c42a171171dbc982bf91b0d43
Rafael Specht <rafaelspdasilva@gmail.com>
2015-01-25 16:51:21 -0200

    adding spaceship animations

--- app/script/spaceship.js
+++ app/script/spaceship.js
@@ -3,16 +3,27 @@ var Spaceship = {
     this.phaser = game.phaser;
     
     // spaceship
-    this.element = this.phaser.add.sprite(40, (this.phaser.stage.height / 2) - 40, 'test');
+    this.element = this.phaser.add.sprite(160, 75, 'spaceship');
     this.phaser.physics.arcade.enable(this.element);
-    this.element.anchor.set(0,0.5);
-    this.element.width = 80;
-    this.element.height = 80;
+    // this.element.anchor.set(0,0.5);
+    // this.element.width = 160;
+    // this.element.height = 75;
     this.element.body.immovable = false;
     this.x = 0;
+
+    this.setAnimations();
     this.setSounds();
 
+    this.element.animations.play('normal');
+
     return this;
+  },
+
+  setAnimations: function () {
+    this.element.animations.add('normal', [0, 1], 4, true, true);
+    this.element.animations.add('boost', [2, 3], 4, true, true);
+    this.element.animations.add('hit', [0, 4], 4, false, true);
+    this.element.animations.add('low-fuel', [5, 6, 5, 6, 7], 10, true, true);
   },
 
   setSounds: function () {
@@ -26,7 +37,10 @@ var Spaceship = {
   render: function() {
     if (this.phaser.input.keyboard.isDown(window.Phaser.Keyboard.SPACEBAR)) {
       this.x += 5;
+      this.element.animations.play('boost');
     } else {
+      this.element.animations.play('normal');
+
       if (this.x <= 10) {
         this.x = 10;
       } else {
