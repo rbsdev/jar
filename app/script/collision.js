f3e507c1de6b6bc0e3064e59ce9680aeeacca89f
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-01-25 11:42:31 -0200

    added debounce function in collision module

--- app/script/collision.js
+++ app/script/collision.js
@@ -1,5 +1,23 @@
 //var Life = require('./life.js');
 
+var debounce = function(func, wait, immediate) {
+  var timeout;
+
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+};
+
 var Collision = {
   initialize: function(enemies) {
     this.enemies = enemies;
@@ -7,10 +25,16 @@ var Collision = {
   },
 
   handler: function(spaceship, element) {
-    var isEnemy = Collision.enemies.indexOf(element.key) !== -1;
-    if (isEnemy) {
-      console.log('it is enemy!');
-    }
+    var onHandler = function() {
+      var isEnemy = Collision.enemies.indexOf(element.key) !== -1;
+
+      if (isEnemy) {
+        console.log('collision!');
+        //Life.decrease();
+      }
+    };
+
+    debounce(onHandler, 250);
   }
 };
 
