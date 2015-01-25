eb4b730608773288ec5daa2978f3a547eceaff3d
Gibran Malheiros <gibatronic@gmail.com>
2015-01-24 22:23:38 -0200

    changed calc for scnenario

--- app/script/scenario.js
+++ app/script/scenario.js
@@ -1,15 +1,23 @@
 var Scenario = {
-  initialize: function(game, width, height) {
-    this.layer3 = game.add.tileSprite(0, 0, width, height, 'layer03');
-    this.layer2 = game.add.tileSprite(0, 0, width, height, 'layer02');
-    this.layer1 = game.add.tileSprite(0, 0, width, height, 'layer01');
+  initialize: function(game) {
+    var scaleX = game.stage.width / 2560;
+    var scaleY = game.stage.height / 1440;
+
+    this.layer3 = game.add.tileSprite(0, 0, game.stage.width, game.stage.height, 'layer03');
+    this.layer2 = game.add.tileSprite(0, 0, game.stage.width, game.stage.height, 'layer02');
+    this.layer1 = game.add.tileSprite(0, 0, game.stage.width, game.stage.height, 'layer01');
 
     this.layer3.anchor.set(0, 0);
     this.layer2.anchor.set(0, 0);
     this.layer1.anchor.set(0, 0);
-    this.layer3.tileScale.y = 0.525;
-    this.layer2.tileScale.y = 0.525;
-    this.layer1.tileScale.y = 0.525;
+
+    this.layer3.tileScale.x = scaleX;
+    this.layer2.tileScale.x = scaleX;
+    this.layer1.tileScale.x = scaleX;
+
+    this.layer3.tileScale.y = scaleY;
+    this.layer2.tileScale.y = scaleY;
+    this.layer1.tileScale.y = scaleY;
   },
 
   render: function() {
